bea4f26ea3cef524ae023bd0f60490a109560959
jahid-hasan-alif <jahid231000@gmail.com>
2025-01-05 12:53:30 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CoachingCenterManagement management = new CoachingCenterManagement();
+        management.start();
+    }
+}
